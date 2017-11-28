de3e73653efa734613002d41e08f7f02c2898388
Jon Smiley <jasmiley0715@Jons-MacBook-Pro.local>
2017-11-28 14:51:19 -0600

    connected starting destination with weather API

--- logic.js
+++ logic.js
@@ -9,6 +9,8 @@
 
 
 
+
+
 $("#add-user").on("click", function(){
 	event.preventDefault();
 	var origin = $("#start-input").val().trim();
@@ -16,7 +18,20 @@ $("#add-user").on("click", function(){
 	// var wTemp = [];
 	var uStartDate = $("#departure-input").val().trim();
 	var uEndDate = $("#return-input").val().trim();
+	var queryURL = "http://api.wunderground.com/api/25befb141962c397/geolookup/conditions/q/iata:" + origin + ".json"
 
+
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+
+	})
+
+	.done(function(response){
+		console.log(response.current_observation.temp_f);
+
+	})
 
 	console.log(origin);
 	console.log(uTemp);
@@ -32,13 +47,26 @@ $("#add-user").on("click", function(){
 	$("#return-input").val("");
 
 
-	
+
 
 // var resultInfo = $("<tr><th>" + trainName + "</th><th>" 
 // 	+ destination + "</th><th>" + frequency + "</th><th>"
 // 	+ arrival + "</th><th>" + minutesAway + "</th></tr>");
 
-	
+//airport 1 and 2 needs to be the three letter airport code
+// var airport1 = ""
+// var airport2 = ""
+//departure date needs to be in the form of "2017-11-28"
+// var departuredate = ""
+// var returndate = ""
+
+// function displaysearchbutton() {
+// 	var url = "https://www.kayak.com/flights/" + airport1 + "-" + airport2 + "/" + departuredate + "/" + returndate;
+// 	window.open(url);
+
+
+// }
+
 
 });
 
